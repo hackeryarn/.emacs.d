b7f2979cf9c961c5b0b2e5d975082bef63574c2a
hackeryarn <artemchernyak@gmail.com>
2019-11-18 18:07:17 -0600

    speed improvements and allowing move by defun

--- custom/packages.el
+++ custom/packages.el
@@ -175,11 +175,9 @@
   (define-key smartparens-mode-map (kbd "C-M-b") 'sp-backward-sexp)
 
   (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
-  (define-key smartparens-mode-map (kbd "C-M-a") 'sp-backward-down-sexp)
   (define-key smartparens-mode-map (kbd "C-S-d") 'sp-beginning-of-sexp)
   (define-key smartparens-mode-map (kbd "C-S-a") 'sp-end-of-sexp)
 
-  (define-key smartparens-mode-map (kbd "C-M-e") 'sp-up-sexp)
   (define-key smartparens-mode-map (kbd "C-M-u") 'sp-backward-up-sexp)
   (define-key smartparens-mode-map (kbd "C-M-t") 'sp-transpose-sexp)
 
